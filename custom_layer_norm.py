18d9b5bf99bf6c31515a22c92177d8fa1f454684
local <local@mv.com>
2021-05-19 11:38:26 +0930

    Add pytorch 1.8 support

--- custom_layer_norm.py
+++ custom_layer_norm.py
@@ -8,40 +8,57 @@ import native
 
 class layer_norm(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x, normalized_shape, weight, bias, eps, training):
-        N = 1
-        if isinstance(normalized_shape, int):
-            N = normalized_shape
-        elif isinstance(normalized_shape, (list, tuple)):
-            for i in normalized_shape:
-                N *= i
-        else:
-            raise RuntimeError("unexpected type of normalized_shape".format(type(normalized_shape)))
-        M = x.nelement() // N
+    def forward(ctx, x, normalized_shape, weight, bias, eps, training=False):
+        if torch.__version__  >= "1.8":
+            if x.is_cuda:
+                y, mean, rstd = native.layer_norm_forward_cuda(x, normalized_shape, weight, bias, eps)
+            else:
+                y, mean, rstd = native.layer_norm_forward_cpu(x, normalized_shape, weight, bias, eps)
 
-        if x.is_cuda:
-
-            y, mean, rstd = native.layer_norm_forward_cuda(x, weight, bias, M, N, eps)
+            if training:
+                ctx.layer_norm_parameters = (mean, rstd, weight, bias, normalized_shape)
         else:
-            y, mean, rstd = native.layer_norm_forward_cpu(x, weight, bias, M, N, eps)
+            N = 1
+            if isinstance(normalized_shape, int):
+                N = normalized_shape
+            elif isinstance(normalized_shape, (list, tuple)):
+                for i in normalized_shape:
+                    N *= i
+            else:
+                raise RuntimeError("type of normalized_shape".format(type(normalized_shape)))
+            M = x.nelement() // N
+
+            if x.is_cuda:
+                y, mean, rstd = native.layer_norm_forward_cuda(x, weight, bias, M, N, eps)
+            else:
+                y, mean, rstd = native.layer_norm_forward_cpu(x, weight, bias, M, N, eps)
+
+            if training:
+                ctx.layer_norm_parameters = (mean, rstd, weight, M, N)
 
         if training:
             ctx.layer_norm_input = x
-            ctx.layer_norm_parameters = (mean, rstd, weight, M, N)
         return y
 
     @staticmethod
     def backward(ctx, grad_output):
         x = ctx.layer_norm_input
-        mean, rstd, weight, M, N = ctx.layer_norm_parameters
 
-        #output_mask = [True, True, True]
-        output_mask = ctx.needs_input_grad #[True, True, True]
-        print(output_mask)
-        if grad_output.is_cuda:
-            grad_input, grad_weight, grad_bias = native.layer_norm_backward_cuda(grad_output, x, mean, rstd, weight, M, N, output_mask)
+        output_mask = [ctx.needs_input_grad[0], ctx.needs_input_grad[2], ctx.needs_input_grad[3]]
+
+        if torch.__version__  >= "1.8":
+            mean, rstd, weight, bias, normalized_shape = ctx.layer_norm_parameters
+            if grad_output.is_cuda:
+                grad_input, grad_weight, grad_bias = native.layer_norm_backward_cuda(grad_output, x, normalized_shape, mean, rstd, weight, bias, output_mask)
+            else:
+                grad_input, grad_weight, grad_bias = native.layer_norm_backward_cpu(grad_output, x, normalized_shape, mean, rstd, weight, bias, output_mask)
         else:
-            grad_input, grad_weight, grad_bias = native.layer_norm_backward_cpu(grad_output, x, mean, rstd, weight, M, N, output_mask)
+            mean, rstd, weight, M, N = ctx.layer_norm_parameters
+
+            if grad_output.is_cuda:
+                grad_input, grad_weight, grad_bias = native.layer_norm_backward_cuda(grad_output, x, mean, rstd, weight, M, N, output_mask)
+            else:
+                grad_input, grad_weight, grad_bias = native.layer_norm_backward_cpu(grad_output, x, mean, rstd, weight, M, N, output_mask)
         ctx.layer_norm_input = None
         ctx.layer_norm_parameters = None
         return grad_input, None, grad_weight, grad_bias, None, None
--- native.cpp
+++ native.cpp
@@ -5,44 +5,94 @@
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor> layer_norm_forward_cpu(
     const at::Tensor & input,
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    at::IntArrayRef normalized_shape,
+#endif
     const at::Tensor & weight,
     const at::Tensor & bias,
-    int64_t M, int64_t N, double eps) {
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+#else
+    int64_t M, int64_t N,
+#endif
+    double eps) {
+
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    return at::native::layer_norm_cpu(input, normalized_shape, weight, bias, eps);
+#else
     return at::native::layer_norm_cpu(input, weight, bias, M, N, eps);
+#endif
 }
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor> backward_layer_norm_cpu(
+std::tuple<at::Tensor, at::Tensor, at::Tensor> layer_norm_backward_cpu(
     const at::Tensor & grad_out,
     const at::Tensor & input,
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    at::IntArrayRef normalized_shape,
+#endif
     const at::Tensor & mean,
     const at::Tensor & rstd,
     const at::Tensor & weight,
-    int64_t M, int64_t N, std::array<bool,3> output_mask) {
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    const at::Tensor & bias,
+#else
+    int64_t M, int64_t N,
+#endif
+    std::array<bool,3> output_mask) {
+
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    return at::native::layer_norm_backward_cpu(grad_out, input, normalized_shape, mean, rstd, weight, bias, output_mask);
+#else
     return at::native::layer_norm_backward_cpu(grad_out, input, mean, rstd, weight, M, N, output_mask);
+#endif
 }
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor> layer_norm_forward_cuda(
     const at::Tensor & input,
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    at::IntArrayRef normalized_shape,
+#endif
     const at::Tensor & weight,
     const at::Tensor & bias,
-    int64_t M, int64_t N, double eps) {
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+#else
+    int64_t M, int64_t N,
+#endif
+    double eps) {
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    return at::native::layer_norm_cuda(input, normalized_shape, weight, bias, eps);
+#else
     return at::native::layer_norm_cuda(input, weight, bias, M, N, eps);
+#endif
 }
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor> backward_layer_norm_cuda(
+std::tuple<at::Tensor, at::Tensor, at::Tensor> layer_norm_backward_cuda(
     const at::Tensor & grad_out,
     const at::Tensor & input,
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    at::IntArrayRef normalized_shape,
+#endif
     const at::Tensor & mean,
     const at::Tensor & rstd,
     const at::Tensor & weight,
-    int64_t M, int64_t N, std::array<bool,3> output_mask) {
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    const at::Tensor & bias,
+#else
+    int64_t M, int64_t N,
+#endif
+    std::array<bool,3> output_mask) {
+
+#if TORCH_VERSION_MAJOR >= 1 && TORCH_VERSION_MINOR >= 8
+    return at::native::layer_norm_backward_cuda(grad_out, input, normalized_shape, mean, rstd, weight, bias, output_mask);
+#else
     return at::native::layer_norm_backward_cuda(grad_out, input, mean, rstd, weight, M, N, output_mask);
+#endif
 }
 
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("layer_norm_forward_cpu",  &layer_norm_forward_cpu, "layer norm forward (cpu version)");
-    m.def("layer_norm_backward_cpu", &backward_layer_norm_cpu, "layer norm backward (cpu version)");
+    m.def("layer_norm_forward_cpu", &layer_norm_forward_cpu, "layer norm forward (cpu version)");
+    m.def("layer_norm_backward_cpu", &layer_norm_backward_cpu, "layer norm backward (cpu version)");
     m.def("layer_norm_forward_cuda", &layer_norm_forward_cuda, "layer norm forward (cuda version)");
-    m.def("layer_norm_backward_cuda",&backward_layer_norm_cuda, "layer norm backward (cuda version)");
+    m.def("layer_norm_backward_cuda", &layer_norm_backward_cuda, "layer norm backward (cuda version)");
 }
 
