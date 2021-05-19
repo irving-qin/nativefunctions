47003506e924ea13e4368b0e1b07e2cbdaebb81c
local <local@mv.com>
2021-05-19 11:39:41 +0930

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ from torch.utils import cpp_extension
 
 compile_args = {"cxx": [], "nvcc": [] }
 
+if torch.__version__ < "1.8":
+    version = torch.__version__.split('.')
+    compile_args['cxx'] += ["-DTORCH_VERSION_MAJOR={}".format(version[0])]
+    compile_args['cxx'] += ["-DTORCH_VERSION_MINOR={}".format(version[1])]
+
 setup(
         name = 'layer_norm_test',
         version = '1.0',
